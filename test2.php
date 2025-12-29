5f6db2edfdbe74b49b40382d0d3cdb8e3211ef09
Priya Singh <priya.s@valuelabs.com>
2025-12-29 10:03:15 +0530

    work on test

--- test2.php
+++ test2.php
@@ -1,48 +1,127 @@
 <?php
 
-namespace Klyp\Nomergy\Services\Stripe;
+namespace Klyp\Nomergy\Http\Controllers\Stripe;
 
-use Carbon\Carbon;
-use Klyp\Nomergy\Models\UserPortal;
+use Klyp\Nomergy\Http\Controllers\ApiController;
+use Symfony\Component\HttpFoundation\Response;
+use Klyp\Nomergy\Services\Stripe\PTBillingProductService;
+use Klyp\Nomergy\Services\Stripe\PTBillingLoggerService;
+use Klyp\Nomergy\Services\Stripe\PTBillingTrainerService;
+use Klyp\Nomergy\Http\Traits\TrainerValidationTrait;
+use Illuminate\Http\JsonResponse;
 
-class PTBillingCustomerPunchCardService
+class ApiPTBillingProductController extends ApiController
 {
-    public function getPunchCardInfoWithHistory(object $user): array
-    {
-        $punchCards = $user->punchCard()
-            ->with(['histories', 'trainer'])
-            ->get();
+    use TrainerValidationTrait;
+    
+    /**
+     * Stripe service instance for product operations.
+     */
+    protected $productService;
 
-        return $punchCards->map(function ($punchCard) {
+    /**
+     * Stripe logger instance.
+     */
+    protected $logger;
 
-            return [
-                'product_name'  => $punchCard->product_name,
-                'trainer_name'  => optional($punchCard->trainer)->first_name . ' ' .
-                                   optional($punchCard->trainer)->last_name,
-                'purchased_at'  => Carbon::parse($punchCard->purchased_at)->format('F d'),
-                'total_session' => $punchCard->total_session,
-                'used_session'  => $punchCard->used_session,
-                'history'       => $this->formatHistory($punchCard->histories),
-            ];
+    /**
+     * Trainer service used to validate the trainer with respect of login user club trainers.
+     */
+    protected $trainerService;
 
-        })->values()->toArray();
+    /**
+     * Inject StripeProductService, .
+     *
+     * @param StripeProductService $stripeProductService The service responsible for interacting with Stripe's Product API.
+     * @param trainerService $trainerService The service responsible for interacting login user club trainers.
+     * @param logger $logger The logger service used to record Stripe events and actions, including user_id.
+     *
+     */
+    public function __construct(
+        PTBillingProductService $productService,
+        PTBillingTrainerService $trainerService,
+        PTBillingLoggerService $logger
+    ) {
+        $this->productService = $productService;
+        $this->trainerService = $trainerService;
+        $this->logger = $logger;
     }
 
-    private function formatHistory($histories): array
+    /**
+     * Get all active products along with their default price
+     * for a given trainer's connected Stripe account.
+     *
+     * @param integer $trainerId
+     * @return JsonResponse
+     */
+    public function getTrainerProducts($trainerId): JsonResponse
     {
-        return $histories->map(function ($history) {
-            $date = Carbon::parse($history->date_of_session);
+        $user = parent::getAuth();
 
-            return [
-                'date'   => $date->format('d M Y'),
-                'time'   => $date->format('h:i A'),
-                'action' => $history->action,
-            ];
-        })->toArray();
+        $this->logger->info("products.fetch", __('klyp.nomergy::fod.fetching_trainer_product'), [
+            'trainer_id' => $trainerId], $user->id ?? null);
+
+        try {
+            $trainer = $this->validateTrainerForUserClub($user, $trainerId, $this->trainerService);
+            $result = $this->productService->getActiveProductsWithDefaultPrice($trainer);
+
+            $this->logger->info("products.fetch.success", __('klyp.nomergy::fod.fetching_trainer_product'), [
+                'trainer_id'    => $trainerId,
+                'product_count' => count($result['products'])
+            ], $user->id ?? null);
+            $this->logger->flush();
+
+            return parent::respond(['status' => 'success',
+                'products' => $result['products'],
+                'trainer' => $result['trainer']
+            ], Response::HTTP_OK);
+        } catch (\Exception $e) {
+            $this->logger->error("products.fetch.error", $e->getMessage(), [], $user->id ?? null);
+            $this->logger->flush();
+
+            return parent::respondError(__('klyp.nomergy::fod.stripe_unexpected_error'), Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 }
 
+=============================
 
- return Cache::remember($cacheKey, now()->addMinutes((int)config('services.stripe.product_cache_time')), function () {
-            return self::fetchStripeProducts();
-        });
+<?php
+
+namespace Klyp\Nomergy\Http\Traits;
+
+use Klyp\Nomergy\Models\UserPortal;
+use Symfony\Component\HttpFoundation\Response;
+
+trait TrainerValidationTrait
+{
+    /**
+     * Validate trainer belongs to the user's club and is onboarded on Stripe.
+     *
+     * @param object $user      Authenticated user
+     * @param int    $trainerId Trainer ID to validate
+     * @param object $trainerService Instance of trainer service for fetching club trainers
+     *
+     * @throws \Illuminate\Http\Exceptions\HttpResponseException
+     */
+    public function validateTrainerForUserClub($user, $trainerId, $trainerService)
+    {
+        $clubId = $user->profile ? $user->profile->club_id : null;
+
+        if (empty($clubId)) {
+            abort(Response::HTTP_BAD_REQUEST, __('klyp.nomergy::fod.user_has_no_club'));
+        }
+
+        $trainers = $trainerService->getTrainerIdsForClub($clubId);
+        if (! in_array($trainerId, $trainers)) {
+            abort(Response::HTTP_BAD_REQUEST, __('klyp.nomergy::fod.trainer_having_different_club_as_login_user'));
+        }
+
+        $trainer = UserPortal::find($trainerId);
+        if (!$trainer || !$trainer->stripe_account_id || !$trainer->is_onboarded) {
+            abort(Response::HTTP_BAD_REQUEST, __('klyp.nomergy::fod.trainer_not_in_stripe'));
+        }
+
+        return $trainer;
+    }
+}
