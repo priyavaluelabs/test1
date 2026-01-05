60bb9a2fae99b20cb6cf2ca4967cc7b8065cfe13
Priya Singh <priya.s@valuelabs.com>
2026-01-05 13:27:05 +0530

    work on test2

--- test2.php
+++ test2.php
@@ -1,52 +1,50 @@
-<?php
+<x-filament-panels::page>
+    <div class="filament-tables-container 
+        rounded-xl border
+        border-gray-300
+        bg-white shadow-sm
+        ">
+        <x-payment-tab />
+    </div>
+    <div class="rounded-xl bg-white dark:bg-gray-900 border border-gray-200 dark:border-gray-700 " wire:ignore>
+        <div class="tax">
+            <!-- Tax Settings -->
+            <div class="p-6 space-y-4">
+                <div class="font-semibold text-[20px] leading-[28px] tracking-[-0.5px] align-middle mb-2">
+                    {{ __('stripe.tax_settings') }}
+                </div>
+                <div id="stripe-tax-loader" class="flex items-center justify-center h-full">
+                    <x-filament::loading-indicator class="h-12 w-12 text-primary-600" />
+                </div>
+                <div id="stripe-tax-setting-container"
+                    data-settings="{{ json_encode([
+                        'publishableKey' => config('services.stripe.key'),
+                        'clientSecret' => $taxSettingsClientSecret,
+                        'type' => $tax_setting_type,
+                        'containerId' => 'stripe-tax-setting-container',
+                        'loaderId' => 'stripe-tax-loader',
+                    ]) }}">
+                </div>
+            </div>
 
-namespace App\Http\Controllers\API;
+            <!-- Tax Registrations -->
+            <div class="p-6 space-y-4">
+                <div class="font-semibold text-[20px] leading-[28px] tracking-[-0.5px] align-middle mb-2">
+                    {{ __('stripe.tax_registrations') }}
+                </div>
+                <div id="stripe-tax-registration-container"
+                    data-settings="{{ json_encode([
+                        'publishableKey' => config('services.stripe.key'),
+                        'clientSecret' => $taxRegistrationsClientSecret,
+                        'type' => $tax_registration_type,
+                        'containerId' => 'stripe-tax-registration-container',
+                        'loaderId' => 'stripe-tax-loader',
+                    ]) }}">
+                </div>
+            </div>
 
-use Stripe\Exception\SignatureVerificationException;
-use App\Events\StripePaymentIntentSucceeded;
-use App\Events\StripeAccountOnboarded;
-use Symfony\Component\HttpFoundation\Response;
-use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Http\Request;
-use Stripe\Webhook;
-
-class StripeWebhookController extends Controller
-{
-    public function handle(Request $request)
-    {
-        $payload = $request->getContent();
-        $sigHeader = $request->header('Stripe-Signature');
-        $endpointSecret = config('services.stripe.webhook_secret');
-
-        if (!$sigHeader) {
-            return response('Missing signature header', Response::HTTP_BAD_REQUEST);
-        }
-
-        try {
-            // Verify the webhook signature
-            $event = Webhook::constructEvent($payload, $sigHeader, $endpointSecret);
-        } catch (SignatureVerificationException $e) {
-            return response('Invalid signature', Response::HTTP_BAD_REQUEST);
-        }
-
-        // Handle the webhook event
-        switch ($event->type) {
-            case 'payment_intent.succeeded':
-                StripePaymentIntentSucceeded::dispatch($event);
-                break;
-
-            case 'invoice.payment_failed':
-                Log::info('Invoice payment failed:', ['event' => $event->toArray()]);
-                break;
-
-            case 'account.updated':
-                StripeAccountOnboarded::dispatch($event);
-                break;
-
-            default:
-        }
-
-        return response('Webhook processed', Response::HTTP_OK);
-    }
-}
+        </div>
+    </div>
+    <x-stripe.terms-and-condition />
+</x-filament-panels::page>
+@vite(['resources/js/stripe-dashboard.js'])
