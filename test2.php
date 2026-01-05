26bf4e8d4902482a8377674e7e7087d273f45c66
Priya Singh <priya.s@valuelabs.com>
2026-01-05 13:42:00 +0530

    work on test2

--- test2.php
+++ test2.php
@@ -1,27 +1,9 @@
-<?php
-
-namespace App\Providers;
-
-use Illuminate\Support\Facades\View;
-use Illuminate\Support\ServiceProvider;
-
-class AppServiceProvider extends ServiceProvider
-{
-    public function register(): void
-    {
-        //
-    }
-
-    public function boot(): void
-    {
-        // Share Stripe publishable key globally
-        View::composer('*', function ($view) {
-            $user = auth()->user();
-            $region = $user?->corporatePartner?->region ?? 'US';
-
-            $publicKey = config("stripe_regions.regions.$region.public");
-
-            $view->with('stripePublicKey', $publicKey);
-        });
-    }
-}
+ <div id="stripe-tax-registration-container"
+                    data-settings="{{ json_encode([
+                        'publishableKey' => config('services.stripe.key'),
+                        'clientSecret' => $taxRegistrationsClientSecret,
+                        'type' => $tax_registration_type,
+                        'containerId' => 'stripe-tax-registration-container',
+                        'loaderId' => 'stripe-tax-loader',
+                    ]) }}">
+                </div>
