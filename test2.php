ba75d55657f2e9c8d29dc505be45bc61d22b2d82
priyavaluelabs <priya.s@valuelabs.com>
2026-01-05 14:22:41 +0530

    Delete test2.php

--- test2.php
+++
@@ -1,9 +0,0 @@
- <div id="stripe-tax-registration-container"
-                    data-settings="{{ json_encode([
-                        'publishableKey' => config('services.stripe.key'),
-                        'clientSecret' => $taxRegistrationsClientSecret,
-                        'type' => $tax_registration_type,
-                        'containerId' => 'stripe-tax-registration-container',
-                        'loaderId' => 'stripe-tax-loader',
-                    ]) }}">
-                </div>
