c679a6d251e06a3a4f27ca4f10178d1aa602a115
Priya Singh <priya.s@valuelabs.com>
2026-01-05 11:52:31 +0530

    work on test2

--- test2.php
+++ test2.php
@@ -1,1 +1,14 @@
 https://stgptbillingportal.liftbrands.com/api/stripe/webhook
+
+
+ /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register(): void
+    {
+        $this->app->singleton(StripeClient::class, function () {
+            return new StripeClient(config('services.stripe.secret'));
+        });
+    }
