ee7e5a47e5ca5deda255ef5cba92e45aad119b6b
Priya Singh <priya.s@valuelabs.com>
2026-01-06 11:33:55 +0530

    work on test

--- test.php
+++ test.php
@@ -2,36 +2,37 @@
 
 namespace Klyp\Nomergy\Services\Stripe;
 
-use Klyp\Nomergy\Services\Stripe\PTbillingBaseStripeService;
-use Klyp\Nomergy\Http\Traits\HasCurrencySymbolTrait;
-use Klyp\Nomergy\Http\Traits\TrainerInfoTrait;
-
-use function Psy\sh;
-
-class PTBillingProductService extends PTbillingBaseStripeService
-{
-    use HasCurrencySymbolTrait, TrainerInfoTrait;
-
-
-
-
-<?php
-
-namespace Klyp\Nomergy\Services\Stripe;
-
 use Stripe\StripeClient;
+use Illuminate\Http\Request;
+use Klyp\Nomergy\Models\UserPortal;
 
-class PTbillingBaseStripeService
+abstract class BaseStripeService
 {
     protected StripeClient $stripe;
 
-    protected function __construct()
+    public function __construct()
     {
-        $key = config("services.stripe.secret_key");
+        $region = 'US';
+        
+        // Get current HTTP request
+        $request = app(Request::class);
+        $trainerId = $request->route('trainerId')
+            ?? $request->route('trainerId')
+            ?? $request->input('trainer_id');
 
-        $this->stripe = new StripeClient($key);
+        if ($trainerId) {
+            $trainer = UserPortal::find($trainerId);
+            if ($trainer) {
+                $corporatePartner = $trainer->getCorporatePartnerForFlexUser($trainer);
+                if($corporatePartner) {
+                    $region = $corporatePartner['region'];
+                }
+            }
+        }
+
+        // Initialize Stripe client
+        $config = config("services.stripe.regions.$region");
+
+        $this->stripe = new StripeClient($config['secret']);
     }
 }
-
-
-[2026-01-06 05:05:06] production.ERROR: Illuminate\Contracts\Container\BindingResolutionException: Target [Klyp\Nomergy\Services\Stripe\PTBillingProductService] is not instantiable while building [Klyp\Nomergy\Http\Controllers\Stripe\ApiPTBillingProductController]. in D:\myproject\liftbrand-app-new\vendor\laravel\framework\src\Illuminate\Container\Container.php:978
