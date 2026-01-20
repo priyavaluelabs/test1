a8e480a0b07abb08e48b810f14716ce367bb7245
priyavaluelabs <priya.s@valuelabs.com>
2026-01-20 10:18:54 +0530

    fgdf

--- coupon.php
+++ coupon.php
@@ -1,35 +1,5 @@
-use Filament\Forms\Components\Select;
-
-
-protected function getStripeProducts(): array
-{
-    $products = $this->stripeClient()
-        ->products
-        ->all(['active' => true, 'limit' => 100]);
-
-    return collect($products->data)
-        ->mapWithKeys(fn ($product) => [
-            $product->id => $product->name,
-        ])
-        ->toArray();
+if (in_array('all', $data['products'] ?? [])) {
+    $products = 'all';
+} else {
+    $products = implode(',', $data['products']);
 }
-
-
-
-
-Select::make('products')
-    ->label('Product(s)')
-    ->statePath('formData.products')
-    ->multiple()
-    ->searchable()
-    ->preload()
-    ->options(fn () => [
-            'all' => 'All Products',
-        ] + $this->getStripeProducts())
-    ->columnSpan(2),
-
-
-'metadata' => [
-    'description' => $data['description'] ?? '',
-    'products' => implode(',', $data['products'] ?? []),
-],
