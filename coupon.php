aea983ce959e3f9e49bf9eed385c665f89744078
priyavaluelabs <priya.s@valuelabs.com>
2026-01-21 16:32:19 +0530

    xcv

--- coupon.php
+++ coupon.php
@@ -1,10 +1,7 @@
-$this->promoCodes[] = [
+$this->promoCodes = collect($this->promoCodes)
+    ->push([
         'id' => $promo->id,
         'code' => $promo->code,
         'active' => $promo->active,
-        'times_redeemed' => $promo->times_redeemed,
-        'max_redemptions' => $promo->max_redemptions,
-        'restrictions' => $promo->expires_at
-            ? Carbon::createFromTimestamp($promo->expires_at)->format('M j, Y')
-            : '—',
-    ];
+    ])
+    ->toArray();
