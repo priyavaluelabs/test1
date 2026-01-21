49f29846ee4c02ee7960982597651db079d2c18d
priyavaluelabs <priya.s@valuelabs.com>
2026-01-21 16:27:52 +0530

    sdf

--- coupon.php
+++ coupon.php
@@ -1,7 +1,10 @@
-$this->promoCodes = collect($this->promoCodes)
-    ->map(fn ($promo) => 
-        $promo['id'] === $promoCodeId
-            ? array_merge($promo, ['active' => $active])
-            : $promo
-    )
-    ->toArray();
+$this->promoCodes[] = [
+        'id' => $promo->id,
+        'code' => $promo->code,
+        'active' => $promo->active,
+        'times_redeemed' => $promo->times_redeemed,
+        'max_redemptions' => $promo->max_redemptions,
+        'restrictions' => $promo->expires_at
+            ? Carbon::createFromTimestamp($promo->expires_at)->format('M j, Y')
+            : '—',
+    ];
