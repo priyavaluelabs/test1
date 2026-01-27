d3cd036d5451ba7e40cfcf57cc573d91b7d645c4
priyavaluelabs <priya.s@valuelabs.com>
2026-01-27 14:34:02 +0530

    dfg

--- coupon.php
+++ coupon.php
@@ -1,12 +1,4 @@
-<x-filament::button 
-    tag="button" 
-    wire:click="goToEditProfile"
-    class="inline-flex items-center hover:text-primary-600 transition">
+<a href="{{ route('filament.admin.pages.edit-profile') }}"
+   class="inline-flex items-center hover:text-primary-600 transition">
     <x-heroicon-o-pencil-square class="w-4 h-4 text-[#444444]" />
-</x-filament::button>
-
-
-public function goToEditProfile()
-{
-    return redirect()->route('filament.admin.pages.edit-profile');
-}
+</a>
