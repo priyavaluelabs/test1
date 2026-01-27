8c81936913624fe7a978025b42c8d7921c42038b
priyavaluelabs <priya.s@valuelabs.com>
2026-01-27 12:55:35 +0530

    jhgbhj

--- coupon.php
+++ coupon.php
@@ -1,15 +1,3 @@
-font-family: Inter;
-font-weight: 600;
-font-style: Semi Bold;
-font-size: Label MD/labelMdFontSize;
-leading-trim: NONE;
-line-height: 20px;
-letter-spacing: 0%;
-
-
-I need above css for 
-
-<span class="text-sm font-medium text-gray-700 px-3">
-                                        {{ $club['club_title'] }}
-                                    </span>
-
+<span class="px-3 text-sm font-semibold leading-5 text-gray-700">
+    {{ $club['club_title'] }}
+</span>
