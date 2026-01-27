3f461f6cac967f93783cab4e8377e550148a2fe1
priyavaluelabs <priya.s@valuelabs.com>
2026-01-27 14:26:21 +0530

    dfgdf

--- coupon.php
+++ coupon.php
@@ -1,11 +1,1 @@
-<p class="mt-8 font-inter font-semibold text-xs leading-5 text-[#444444]">
-    Note:
-</p>
-<b>
-                
-
-<p class="mt-10 text-xs leading-5 text-[#444444]">
-    We automatically sync your bio and photo from Glofox.
-    If you would like to make updates, please do so in Glofox.
-</p>
-
+<x-heroicon-o-pencil-square class="w-4 h-4 text-gray-700" />
