d495cd1858c4dc2218153e23a33213a48d15bde0
priyavaluelabs <priya.s@valuelabs.com>
2026-01-27 13:29:05 +0530

    dfg

--- coupon.php
+++ coupon.php
@@ -1,5 +1,19 @@
-<span class="px-4 py-2
-                                                rounded-full text-sm font-semibold
-                                                bg-red-100 text-red-700">
-                                            Not Verified
-                                        </span>
+<span
+    class="inline-flex items-center gap-2
+           px-4 py-2
+           rounded-full
+           text-sm font-semibold
+           bg-red-100
+           text-red-700">
+
+    <span
+        class="flex items-center justify-center
+               w-6 h-6
+               rounded-full
+               border-2 border-red-700">
+
+        <x-heroicon-s-x-circle class="w-4 h-4 text-red-700" />
+    </span>
+
+    Not Verified
+</span>
