333645a4e1d33c20c320b5d3b7c61f45e687e124
priyavaluelabs <priya.s@valuelabs.com>
2026-01-27 13:27:35 +0530

    sdfg

--- coupon.php
+++ coupon.php
@@ -1,21 +1,5 @@
-<span
-    class="inline-flex items-center gap-2
-           px-4 py-2
-           rounded-full
-           border
-           bg-[#F3F8EE]
-           border-[#C7D7AE]
-           text-sm font-semibold
-           text-[#4D7C0F]">
-
-    <span
-        class="flex items-center justify-center
-               w-6 h-6
-               rounded-full
-               border-2 border-[#4D7C0F]">
-
-        <x-heroicon-o-check class="w-3.5 h-3.5 text-[#4D7C0F]" />
-    </span>
-
-    Glofox Verified
-</span>
+<span class="px-4 py-2
+                                                rounded-full text-sm font-semibold
+                                                bg-red-100 text-red-700">
+                                            Not Verified
+                                        </span>
