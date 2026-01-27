5751e3bd08ac8010fa1adce8ec6878ea0954f3ac
priyavaluelabs <priya.s@valuelabs.com>
2026-01-27 10:54:42 +0530

    xcvc

--- coupon.php
+++ coupon.php
@@ -1,27 +1,8 @@
-font-family: Inter;
-font-weight: 700;
-font-style: Bold;
-font-size: Heading XL/headingXlFontSize;
-leading-trim: NONE;
-line-height: 36px;
-letter-spacing: 1.5%;
-vertical-align: middle;
+<h2 class="text-2xl font-bold leading-9 tracking-wide text-gray-900">
+    Trainer Verification
+</h2>
 
-<h2 class="text-xl font-semibold text-gray-900">
-                            Trainer Verification
-                        </h2>
 
-font-family: SF Pro;
-font-weight: Body MD/bodyMdFontWeight;
-font-style: Regular;
-font-size: Body MD/bodyMdFontSize;
-leading-trim: NONE;
-line-height: 24px;
-letter-spacing: 0%;
-vertical-align: middle;
-#888888
-
-<p class="text-sm text-gray-600">
-                            Let's make sure you're connected.
-                        </p>
-
+<p class="text-sm font-normal leading-6 text-gray-500">
+    Let's make sure you're connected.
+</p>
