a840ce9122e99e3b32b2bbd0fb7924bc330f99b7
priyavaluelabs <priya.s@valuelabs.com>
2026-01-27 14:31:38 +0530

    dfgfd

--- coupon.php
+++ coupon.php
@@ -1,1 +1,12 @@
-<x-heroicon-o-pencil-square class="w-4 h-4 text-gray-700" />
+<x-filament::button 
+    tag="button" 
+    wire:click="goToEditProfile"
+    class="inline-flex items-center hover:text-primary-600 transition">
+    <x-heroicon-o-pencil-square class="w-4 h-4 text-[#444444]" />
+</x-filament::button>
+
+
+public function goToEditProfile()
+{
+    return redirect()->route('filament.admin.pages.edit-profile');
+}
