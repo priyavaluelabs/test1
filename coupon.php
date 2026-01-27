203c664dae125963d7c2ccc3b45a3835924fb8d1
priyavaluelabs <priya.s@valuelabs.com>
2026-01-27 10:58:31 +0530

    fgfd

--- coupon.php
+++ coupon.php
@@ -1,8 +1,33 @@
-<h2 class="text-2xl font-bold leading-9 tracking-wide text-gray-900">
-    Trainer Verification
-</h2>
+font-family: Inter;
+font-weight: 600;
+font-style: Semi Bold;
+font-size: Label MD/labelMdFontSize;
+leading-trim: NONE;
+line-height: 20px;
+letter-spacing: 0%;
+color : #444444
+
+ <span class="text-sm font-medium text-gray-700">Your Account</span>
+
+border-radius: 8px;
+padding-top: 6px;
+padding-right: 12px;
+padding-bottom: 6px;
+padding-left: 12px;
+background color : #F7F7F7
 
 
-<p class="text-sm font-normal leading-6 text-gray-500">
-    Let's make sure you're connected.
-</p>
+font-family: IBM Plex Mono;
+font-weight: 400;
+font-style: Regular;
+font-size: 12px;
+leading-trim: NONE;
+line-height: 20px;
+letter-spacing: 0%;
+
+color : #444444
+<span class="px-3 py-1 text-sm bg-gray-100 rounded-md">
+                                    {{ $user->email }}
+                                </span>
+
+ 
