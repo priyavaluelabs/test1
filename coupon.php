82d94c404a1dae09f8d0c306ab7b9bcc1e66bb86
priyavaluelabs <priya.s@valuelabs.com>
2026-01-27 10:24:19 +0530

    cfgf

--- coupon.php
+++ coupon.php
@@ -1,12 +1,18 @@
-$clubs = FodUserRole::with('club:id,title')
-    ->where('user_id', $this->user->id)
-    ->whereIn('club_id', $this->user->getAccessibleClubs())
-    ->get()
-    ->map(function ($role) {
-        return [
-            'club_title' => $role->club->title ?? null,
-            'glofox_verified_at' => $role->glofox_verified_at,
-        ];
-    });
+$accessibleClubIds = $this->user->getAccessibleClubs();
 
-dd($clubs);
+$clubs = Club::whereIn('id', $accessibleClubIds)
+    ->pluck('title', 'id'); // [id => title]
+
+$userRoles = FodUserRole::where('user_id', $this->user->id)
+    ->whereIn('club_id', $accessibleClubIds)
+    ->get(['club_id', 'glofox_verified_at']);
+
+$result = $userRoles->map(function ($role) use ($clubs) {
+    return [
+        'club_id'            => $role->club_id,
+        'club_title'         => $clubs[$role->club_id] ?? null,
+        'glofox_verified_at' => $role->glofox_verified_at,
+    ];
+});
+
+dd($result);
