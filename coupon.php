90d7bdd8ec2741281010e6064a8500e8cda49039
priyavaluelabs <priya.s@valuelabs.com>
2026-01-28 16:29:36 +0530

    hgfvh

--- coupon.php
+++ coupon.php
@@ -1,9 +1,7 @@
-use Stripe\StripeClient;
-
-$stripe = new StripeClient(config('services.stripe.secret'));
-
-$product = $stripe->products->retrieve(
-    $productId,
-    [], // no params
-    ['stripe_account' => $stripeAccountId] // optional, for Connect
-);
+Forms\Components\Grid::make(4)->schema([
+                    Forms\Components\TextInput::make('name')
+                        ->label(__('stripe.coupon_name'))
+                        ->required()
+                        ->maxLength(255)
+                        ->columnSpan(2),
+                ]),
