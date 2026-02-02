ab7c1942069abc3ab5900104901764059dba379c
priyavaluelabs <priya.s@valuelabs.com>
2026-02-02 13:18:16 +0530

    hgfh

--- coupon.php
+++ coupon.php
@@ -1,15 +1,1 @@
- // Update PaymentIntent metadata
-        $paymentIntent = $this->updatePaymentIntentMetadata(
-            $finalInvoice->payment_intent,
-            $data['trainer_name'],
-            $product,
-            $amount,
-            $stripeAccount
-        );
-
-
-=///
-
-
-protected function updatePaymentIntentMetadata(string $paymentIntentId, string $trainerName, $product, int $amount, string $stripeAccount)
-    {
+"error": "Argument 1 passed to Klyp\\Nomergy\\Services\\Stripe\\PTBillingCustomerService::updatePaymentIntentMetadata() must be of the type string, null given, called in
