74ec4c7229e5e355a023684c32b6fd6248f8b56d
priyavaluelabs <priya.s@valuelabs.com>
2026-02-02 17:18:05 +0530

    dfgfd

--- coupon.php
+++ coupon.php
@@ -4,3 +4,14 @@ Forms\Components\Select::make('customer_id')
                     ->options(fn () => $this->getStripeCustomers())
                     ->placeholder(__('stripe.all_customers'))
                     ->nullable(),
+
+
+Forms\Components\Select::make('customer_id')
+    ->label(__('stripe.restrict_customer'))
+    ->searchable()
+    ->options(fn () => $this->getStripeCustomers())
+    ->placeholder(__('stripe.all_customers'))
+    ->nullable()
+    ->extraInputAttributes([
+        'class' => 'max-w-full overflow-hidden text-ellipsis whitespace-nowrap',
+    ]);
