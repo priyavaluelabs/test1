79a2152fe000651ac99e5e8ab9fd77a9d379b3f4
priyavaluelabs <priya.s@valuelabs.com>
2026-02-03 12:16:22 +0530

    dgdfg

--- coupon.php
+++ coupon.php
@@ -1,11 +1,49 @@
-$paymentIntent = \Stripe\PaymentIntent::retrieve($paymentIntentId);
+@component('emails.layouts.template', [
+    'previewText' => __('mail.customer_purchase.preview_text_short')
+])
+    <table class="module" role="module" data-type="text" border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout: fixed;">
+        <tbody>
+            <tr>
+                <td bgcolor="white">
+                    <div>
+                        <p style="color: #8B1E2D; font-weight: 600; font-size:16px; padding-bottom: 16px;">
+                            {{ __('mail.customer_purchase.heading') }}
+                        </p>
 
-// Get first charge (most PaymentIntents have 1)
-$charge = $paymentIntent->charges->data[0] ?? null;
+                        @foreach([
+                            __('mail.customer_purchase.greeting', ['name' => $customerName]),
+                            __('mail.customer_purchase.thank_you'),
+                        ] as $paragraph)
+                            <p style="padding-bottom: 16px;">
+                                {{ $paragraph }}
+                            </p>
+                        @endforeach
 
-if ($charge && isset($charge->payment_method_details->card)) {
-    $card = $charge->payment_method_details->card;
-    $brand = $card->brand; // e.g., Visa, Mastercard
-    $last4 = $card->last4;
-    echo "{$brand} ending in {$last4}";
-}
+                        <div style="padding-bottom: 16px;">
+                            <strong style="padding-bottom:16px;">{{ __('mail.customer_purchase.order_details_title') }}</strong>
+                            <div class="purchase-summary" style="color: #888888; padding-left:25px;">
+                                <p style="padding-bottom:10px; padding-top:10px;">{{ $productName }}</p>
+                                <p style="padding-bottom:10px;">{{ $trainerName }}<p>
+                                <p style="padding-bottom:10px;">{{ $amount }}<p>
+                                <p style="padding-bottom:10px;">{{ ucfirst($paymentMethod) }}<p>
+                                <p>{{ $purchasedAt->format('F jS Y, h:i A') }}<p>
+                            </div>
+                        </div>
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <div>
+                        <p style="padding-bottom: 16px;">
+                            {{ __('mail.customer_purchase.automated_note', ['trainer' => $trainerName]) }}
+                        </p>
+                        <p style="padding-bottom: 8px;">
+                            {{ __('mail.thank_you') }}
+                        </p>
+                    </div>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+@endcomponent
