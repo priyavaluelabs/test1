338ccdaea01262f4ad92a66b340c329b0f2c32d7
priyavaluelabs <priya.s@valuelabs.com>
2026-02-03 10:07:27 +0530

    dfgd
    bvcv

--- coupon.php
+++ coupon.php
@@ -1,35 +1,6 @@
 @component('emails.layouts.template')
-    <table class="module" role="module" data-type="text" border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout: fixed;">
-        <tbody>
-            <tr>
-                <td bgcolor="white" style="padding:10px;">
-                    <div>
-                        <p style="color: #8B1E2D; font-weight: 600; padding-bottom: 16px;">
-                            {{ __('mail.trainer_onboarding.preview_text') }}
-                        </p>
-                        @foreach([
-                            __('mail.trainer_onboarding.intro', ['name' => $user->name]),
-                            __('mail.trainer_onboarding.intro1'),
-                            __('mail.trainer_onboarding.setup_info'),
-                            __('mail.trainer_onboarding.manager_help'),
-                            __('mail.thank_you'),
-                        ] as $paragraph)
-                            <p style="padding-bottom: 16px;">
-                                {{ $paragraph }}
-                            </p>
-                        @endforeach
-                    </div>
-                </td>
-            </tr>
-            <tr>
-                <td style="padding:0 10px 10px 10px;">
-                    <div>
-                        <p style="padding-bottom: 16px; font-style: italic">
-                            {{ __('mail.trainer_onboarding.footer') }}
-                        </p>
-                    </div>
-                </td>
-            </tr>
-        </tbody>
-    </table>
-@endcomponent
+
+{{-- Hidden preview text --}}
+<span style="display:none !important; visibility:hidden; mso-hide:all; font-size:1px; color:#ffffff; line-height:1px; max-height:0; max-width:0; opacity:0; overflow:hidden;">
+    {{ __('mail.trainer_onboarding.preview_text') }}
+</span>
