09965486207427b026c457d5d8890d5ea8df1270
priyavaluelabs <priya.s@valuelabs.com>
2026-02-03 11:42:24 +0530

    dsgdsg

--- coupon.php
+++ coupon.php
@@ -1,2 +1,11 @@
-  // Get payment method
-            $paymentMethod = $paymentIntent->payment_method_types[0] ?? 'card';
+$paymentIntent = \Stripe\PaymentIntent::retrieve($paymentIntentId);
+
+// Get first charge (most PaymentIntents have 1)
+$charge = $paymentIntent->charges->data[0] ?? null;
+
+if ($charge && isset($charge->payment_method_details->card)) {
+    $card = $charge->payment_method_details->card;
+    $brand = $card->brand; // e.g., Visa, Mastercard
+    $last4 = $card->last4;
+    echo "{$brand} ending in {$last4}";
+}
