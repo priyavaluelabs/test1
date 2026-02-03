9464e6d1f348084eec0624a4cfedcc978898e376
priyavaluelabs <priya.s@valuelabs.com>
2026-02-03 11:35:09 +0530

    dfgfdg

--- coupon.php
+++ coupon.php
@@ -1,12 +1,1 @@
- <!-- PREHEADER / PREVIEW TEXT -->
-    <div style="
-        color:#ffffff;
-        font-size:1px;
-        line-height:1px;
-        max-height:1px;
-        max-width:1px;
-        opacity:0;
-        overflow:hidden;
-    ">
-        Take your next steps today&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;
-    </div>
+Card [[is this just the pmt method, should it be “card ending in 1234”?]]
